6b4ac0285e96a942c081d3737d9a94bfb02affd2
chvinne10 <chvinaykumarchviti@gmail.com>
2026-03-11 19:34:24 +0530

    deployment server

--- config/settings.py
+++ config/settings.py
@@ -70,19 +70,18 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'config.wsgi.application'
+import os
+import dj_database_url
 
+# ... your other settings ...
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+# This tells Django: "Look for DATABASE_URL in Render's environment. 
+# If you don't find it, fall back to the local Docker 'db' setup."
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'student_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'postgres://postgres:postgres@db:5432/student_db'),
+        conn_max_age=600
+    )
 }
 
 
